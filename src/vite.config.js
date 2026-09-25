90774801c4da64ad3fb690bd0c6e4f40766b2e40
bokuran <nartov.96@icloud.com>
2026-09-25 09:54:24 +0300

    Fix base path and add .nojekyll

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
   // 👇 ДОБАВЬТЕ ЭТУ СТРОКУ
-  base: '/Bokuran/', // Замените my-vite-app на имя вашего репозитория на GitHub
+  base: '/dobr/', 
   
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
     },
   },
-  // ... остальные настройки
+  // ... остальное
 })
