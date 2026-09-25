ae67331e1e2a96f77d79aceb2d4d773ab16ffe19
bokuran <nartov.96@icloud.com>
2026-09-25 10:11:59 +0300

    Fix config location and dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  // 👇 Это самая важная строка, из-за которой всё сломалось
   base: '/dobr/', 
-  
-  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
